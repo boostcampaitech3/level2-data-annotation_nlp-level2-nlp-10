c20f75cf835d3413f926116e6b78fd68c6c53326
greenare <greenare_@naver.com>
2022-04-22 05:15:37 +0000

    adjust code

--- code/load_data.py
+++ code/load_data.py
@@ -1,5 +1,10 @@
+import pickle as pickle
+import os
 import pandas as pd
+from df_edit import better_df
 import torch
+
+from sklearn.model_selection import train_test_split
 
 class RE_Dataset(torch.utils.data.Dataset):
   """ Dataset 구성을 위한 class."""
@@ -17,18 +22,23 @@ class RE_Dataset(torch.utils.data.Dataset):
 
 # Typed entity marker(punct) to Subject/Object Entity and Sentence
 def preprocessing_dataset_with_sentence(dataset : pd.DataFrame):
+  """ Sentence 및 Subject/Object Entity에 Typed entity marker를 추가합니다.
+  Args:
+      dataset (DataFrame): 전처리 및 개선 작업 완료한 DataFrame
+  Returns:
+      DataFrame: Typed entity marker를 추가한 DataFrame
+  """  
   subject_entity = []
   object_entity = []
   sentence = []
-  for SEN, SE, OE in zip( dataset['sentence'], dataset['subject_entity'], dataset['object_entity']):
-    S_WORD = SE['word']
-    S_TYPE = SE['type']
+  for S_WORD,S_TYPE,O_WORD,O_TYPE,SEN in zip(dataset['subj_word'], dataset['subj_type'], dataset['obj_word'], dataset['obj_type'], dataset['sentence']): 
+    
     S_TEMP = ' '.join(['@', '*', '['+S_TYPE+']', '*', S_WORD, '@'])
+    # S_TEMP = ' '.join(['@', '+', S_TYPE, '+', S_WORD, '@'])
     subject_entity.append(S_TEMP)
-
-    O_WORD = OE['word']
-    O_TYPE = OE['type']
+  
     O_TEMP = ' '.join(['#', '^', '['+O_TYPE+']', '^', O_WORD, '#'])
+    # O_TEMP = ' '.join(['#', '^', O_TYPE, '^', O_WORD, '#'])
     object_entity.append(O_TEMP)
 
     sentence.append(SEN.replace(S_WORD, S_TEMP).replace(O_WORD, O_TEMP))
@@ -37,13 +47,64 @@ def preprocessing_dataset_with_sentence(dataset : pd.DataFrame):
   return out_dataset
 
 def load_data(dataset_dir : str):
+  """
+  Train 용 dataframe 생성 함수
+  1) csv 파일을 경로에 맞게 불러 옵니다. 
+  2) dataframe을 개선하고 전처리 과정을 수행합니다.
+  3) type + 특수문자를 Subj/Obj Entity + Sentence에 추가합니다.
+  Args:
+      dataset_dir (file_dir): train data의 위치 (default : "/opt/ml/dataset/train/train.csv")
+  Returns:
+      type: Pandas Dataframe
+  """  
+
+  # 주어진 경로에서 csv 파일을 불러와 dataframe으로 읽습니다.
   pd_dataset = pd.read_csv(dataset_dir)
+
+  # dataframe 개선을 합니다. word, index, type을 풀어 각각 하나의 column으로 담습니다. label num을 포함합니다.
+  dataset = better_df(pd_dataset,0)
+  
+  # 모든 값이 일치한 data를 삭제합니다. 또한 label만 다른 data 쌍에 대해 합의를 통해 삭제할 대상을 선정하여 삭제합니다.
+
+  # 특수 문자+ type을 subj, obj entity + 문장에 추가합니다.
+  # 논문 An Improved Baseline for Sentence-level Relation Extraction (2021) 참고
+  dataset = preprocessing_dataset_with_sentence(dataset)
+
+  return dataset
+
+
+def load_data_test(dataset_dir : str):
+  """
+  inference용 dataframe 생성 함수
+  1) csv 파일을 경로에 맞게 불러 옵니다. 
+  2) inference 할 때는 전처리를 하지 않습니다.
+  3) type + 특수문자를 Subj/Obj Entity + Sentence에 추가합니다.
+  Returns:
+      type : Pandas Dataframe
+  """  
+  # inference 할 때는 전처리를 하지 않습니다.
+  pd_dataset = pd.read_csv(dataset_dir)
+
+  # dataframe 개선을 합니다. word, index, type을 풀어 각각 하나의 column으로 담습니다. label_num은 추가하지 않습니다. 
+  pd_dataset = better_df(pd_dataset,1)
+  
+  # 특수 문자+ type을 subj, obj entity + 문장에 추가합니다.
+  # 논문 An Improved Baseline for Sentence-level Relation Extraction (2021) 참고
   dataset = preprocessing_dataset_with_sentence(pd_dataset)
 
   return dataset
 
 
 def tokenized_dataset(dataset : pd.DataFrame, tokenizer):
+  """ 
+  Query를 기존 BERT 학습 방법과 동일하게 한 문장으로 구성합니다. 주어진 tokenizer에 따라 sentence를 tokenizing 합니다.
+  max_length = 256으로 설정되어 있습니다. 본인이 필요에 따라 바꾸시면 됩니다.
+  Args:
+      dataset (DataFrame): 개선, 전처리, 문장에 type entity marker를 추가한 dataset
+      tokenizer (AutoTokenizer): 주어진 tokenizer
+  Returns:
+      list: 토큰화된 문장
+  """  
   concat_entity = []
   for e01, e02 in zip(dataset['subject_entity'], dataset['object_entity']):
     temp = e01 + ' 과 ' + e02 + '의 관계'
--- code/train.py
+++ code/train.py
@@ -58,11 +58,10 @@ class CustomTrainer(Trainer):
         if self.args.past_index >=0:
             self._past= outputs[self.args.past_index]
 
-        custom_loss = torch.nn.CrossEntropyLoss.to(device)
+        custom_loss = torch.nn.CrossEntropyLoss().to(device)
         loss = custom_loss(outputs['logits'], labels)    
         return (loss, outputs) if return_outputs else loss
-        
-
+  
 def klue_re_micro_f1(preds, labels):
     """KLUE-RE micro f1 (except no_relation)"""
     label_list = ['no_relation', 'per:production', 'per:title', 'org:production', 'com:date_of_produced',
@@ -111,6 +110,7 @@ def label_to_num(label):
 
   return num_label
 
+
 def train():
 
   # load model and tokenizer
@@ -120,35 +120,39 @@ def train():
   
   # load dataset
   train_dataset = load_data("./train.csv")
-
   train_label = label_to_num(train_dataset['label'].values)
-
-  # tokenizing dataset
   tokenized_train = tokenized_dataset(train_dataset, tokenizer)
-
+  
+  # eval_dataset = load_test_data("./test.csv")
+  # eval_label = label_to_num(eval_dataset['answer'].values)
+  # tokenized_eval = tokenized_dataset(eval_dataset, tokenizer)
+  
   # make dataset for pytorch.
   RE_train_dataset = RE_Dataset(tokenized_train, train_label)
+  # RE_eval_dataset = RE_Dataset(tokenized_eval, eval_label)
+ 
   device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-  model =  Model(MODEL_NAME)
+  model = Model(MODEL_NAME)
   model.model.resize_token_embeddings(tokenizer.vocab_size + added_token_num)
   model.to(device)
 
   training_args = TrainingArguments(
     output_dir='./results',          # output directory
-    save_strategy='no',
+    save_strategy='steps',
     save_total_limit=1,              # number of total save model.
-    num_train_epochs=1,              # total number of training epochs
+    num_train_epochs=2,              # total number of training epochs
     learning_rate=3e-6,               # learning_rate
-    per_device_train_batch_size=64,  # batch size per device during training
-    per_device_eval_batch_size=64,   # batch size for evaluation
+    per_device_train_batch_size=17,  # batch size per device during training
+    per_device_eval_batch_size=17,   # batch size for evaluation
     warmup_ratio = 0.1,
     weight_decay=0.01,               # strength of weight decay
     label_smoothing_factor=0.1,
-    # lr_scheduler_type = 'cosine',
     logging_dir='./logs',            # directory for storing logs
-    logging_steps=100,              # log saving step.
-    evaluation_strategy='no', # evaluation strategy to adopt during training
+    logging_strategy='steps',
+    eval_steps = 10,
+    logging_steps=50,              # log saving step.
+    evaluation_strategy='steps', # evaluation strategy to adopt during training
                                 # `no`: No evaluation during training.
                                 # `steps`: Evaluate every `eval_steps`.
                                 # `epoch`: Evaluate every end of epoch.
@@ -161,8 +165,8 @@ def train():
     model=model,                         # the instantiated 🤗 Transformers model to be trained
     args=training_args,                  # training arguments, defined above
     train_dataset=RE_train_dataset,         # training dataset
-    # eval_dataset=RE_train_dataset,             # evaluation dataset
-    compute_metrics=compute_metrics         # define metrics function
+    eval_dataset=RE_train_dataset,
+    compute_metrics=compute_metrics
   )
 
   trainer.train()
@@ -173,8 +177,7 @@ def main():
 
 if __name__ == '__main__':
   wandb.init(project="ANNOTATION")
-  wandb.run.name = f'annotation_test'
+  wandb.run.name = 'annotation_test'
   seed_everything(42)
-  # os.environ["WANDB_DISABLED"] = "true"
   
   main()
